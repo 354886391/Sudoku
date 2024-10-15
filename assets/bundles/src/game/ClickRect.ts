de429fabc29063154af50da2805d831d40258ada
Ancolo <miogo@sina.com>
2024-10-16 01:03:12 +0800

    update

---
+++ assets/bundles/src/game/ClickRect.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ClickGroup')
+export class ClickGroup extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
