3cbb36103a03ce15e5451c2b9cb2ad000e491a5e
Ancolo <miogo@sina.com>
2024-10-17 17:07:14 +0800

    optimize draw graphics

--- assets/bundles/src/game/DrawView.ts
+++ assets/bundles/src/game/DrawView.ts
@@ -12,7 +12,6 @@ export class DrawView extends Component {
     draw2: Graphics = null;
 
     protected start(): void {
-
         let itemWidth = 112;
         //#region 绘制棋盘
         let gapWidth = 4;
@@ -21,20 +20,12 @@ export class DrawView extends Component {
         let viewWidth = rectWidth * 3;
 
         this.draw.lineWidth = lineWidth;
-        // 矩形边框
+        // 边
         this.draw.rect(-viewWidth / 2, -viewWidth / 2, viewWidth, viewWidth);
-        // 横1
-        this.draw.moveTo(-viewWidth / 2, rectWidth / 2);
-        this.draw.lineTo(viewWidth / 2, rectWidth / 2);
-        // 横2
-        this.draw.moveTo(-viewWidth / 2, -rectWidth / 2);
-        this.draw.lineTo(viewWidth / 2, -rectWidth / 2);
-        // 竖1
-        this.draw.moveTo(-rectWidth / 2, viewWidth / 2);
-        this.draw.lineTo(-rectWidth / 2, -viewWidth / 2);
-        // 竖2
-        this.draw.moveTo(rectWidth / 2, viewWidth / 2);
-        this.draw.lineTo(rectWidth / 2, -viewWidth / 2);
+        // 横
+        this.draw.rect(-viewWidth / 2, -rectWidth / 2, viewWidth, rectWidth);
+        // 竖
+        this.draw.rect(-rectWidth / 2, -viewWidth / 2, rectWidth, viewWidth);
         this.draw.stroke();
         //#endregion
 
@@ -43,39 +34,16 @@ export class DrawView extends Component {
         let pos1 = itemWidth + gapWidth / 2;
         let pos2 = itemWidth + pos1;
         this.draw2.lineWidth = lineWidth2;
-        // 横1
-        this.draw2.moveTo(-viewWidth / 2, rectWidth / 2 + pos1);
-        this.draw2.lineTo(viewWidth / 2, rectWidth / 2 + pos1);
-        this.draw2.moveTo(-viewWidth / 2, rectWidth / 2 + pos2);
-        this.draw2.lineTo(viewWidth / 2, rectWidth / 2 + pos2);
-        // 横2
-        this.draw2.moveTo(-viewWidth / 2, itemWidth / 2);
-        this.draw2.lineTo(viewWidth / 2, itemWidth / 2);
-        this.draw2.moveTo(-viewWidth / 2, -itemWidth / 2);
-        this.draw2.lineTo(viewWidth / 2, -itemWidth / 2);
-        // 横3
-        this.draw2.moveTo(-viewWidth / 2, -rectWidth / 2 - pos1);
-        this.draw2.lineTo(viewWidth / 2, -rectWidth / 2 - pos1);
-        this.draw2.moveTo(-viewWidth / 2, -rectWidth / 2 - pos2);
-        this.draw2.lineTo(viewWidth / 2, -rectWidth / 2 - pos2);
-        // 竖1
-        this.draw2.moveTo(-rectWidth / 2 - pos1, viewWidth / 2);
-        this.draw2.lineTo(-rectWidth / 2 - pos1, -viewWidth / 2);
-        this.draw2.moveTo(-rectWidth / 2 - pos2, viewWidth / 2);
-        this.draw2.lineTo(-rectWidth / 2 - pos2, -viewWidth / 2);
-        // 竖2
-        this.draw2.moveTo(itemWidth / 2, viewWidth / 2);
-        this.draw2.lineTo(itemWidth / 2, -viewWidth / 2);
-        this.draw2.moveTo(-itemWidth / 2, viewWidth / 2);
-        this.draw2.lineTo(-itemWidth / 2, -viewWidth / 2);
-        // 竖3
-        this.draw2.moveTo(rectWidth / 2 + pos1, viewWidth / 2);
-        this.draw2.lineTo(rectWidth / 2 + pos1, -viewWidth / 2);
-        this.draw2.moveTo(rectWidth / 2 + pos2, viewWidth / 2);
-        this.draw2.lineTo(rectWidth / 2 + pos2, -viewWidth / 2);
+        // 横
+        this.draw2.rect(-viewWidth / 2,  rectWidth / 2 + pos1, viewWidth, itemWidth);
+        this.draw2.rect(-viewWidth / 2, -itemWidth / 2       , viewWidth, itemWidth);
+        this.draw2.rect(-viewWidth / 2, -rectWidth / 2 - pos2, viewWidth, itemWidth);
+        // 竖
+        this.draw2.rect(-rectWidth / 2 - pos2, -viewWidth / 2, itemWidth, viewWidth);
+        this.draw2.rect(-itemWidth / 2       , -viewWidth / 2, itemWidth, viewWidth);
+        this.draw2.rect( rectWidth / 2 + pos1, -viewWidth / 2, itemWidth, viewWidth);
         this.draw2.stroke();
         //#endregion
-
     }
 
 }
