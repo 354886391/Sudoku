f67b293f76a6711bd58b8d57117d76e35233865e
Ancolo <miogo@sina.com>
2024-11-04 23:52:19 +0800

    update

---
+++ assets/bundles/src/tool/Sudoku.ts
@@ -0,0 +1,15 @@
+import { Singleton } from "../../../script/framework/util/Singleton";
+
+export class Sudoku extends Singleton<Sudoku>() {
+
+
+    init(): void{
+
+    }
+
+    generate(difficulty, unique){
+
+    }
+
+    
+}
